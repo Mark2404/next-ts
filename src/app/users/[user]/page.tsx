c2daea7570b6588716386f6e55209e56d6c56564
Javohir <markpomidorchik@gmail.com>
2025-03-24 17:52:25 +0500

    params deploy error

--- src/app/users/[user]/page.tsx
+++ src/app/users/[user]/page.tsx
@@ -1,25 +1,27 @@
 import { notFound } from "next/navigation";
 
 type Props = {
-    params: { user: string };
+    params: Promise<{ user: string }>;
 };
 
 export default async function UserDetails({ params }: Props) {
+    const { user } = await params;
+
     try {
-        const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.user}`, {
+        const res = await fetch(`https://jsonplaceholder.typicode.com/users/${user}`, {
             cache: "no-store",
         });
 
         if (!res.ok) return notFound();
 
-        const user = await res.json();
+        const userData = await res.json();
 
         return (
             <div className="user-details">
-                <h1>{user.name}</h1>
-                <p>Email: {user.email}</p>
-                <p>Phone: {user.phone}</p>
-                <p>Website: {user.website}</p>
+                <h1>{userData.name}</h1>
+                <p>Email: {userData.email}</p>
+                <p>Phone: {userData.phone}</p>
+                <p>Website: {userData.website}</p>
                 <a href="/">← Back to list</a>
             </div>
         );
